b0157029a494a79c5e15208701bd8e876c4ac63f
nonergodic <nonergodic@gmail.com>
2026-04-06 11:22:56 -0700

    update python feature

--- devcontainer/features/python-packages/install.sh
+++ devcontainer/features/python-packages/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eu
 
-su vscode -c 'pip install --user jupyter ipykernel numpy ipywidgets scipy matplotlib'
+su vscode -c 'pip install --user jupyter ipykernel numpy scipy pandas ipywidgets matplotlib plotly'
 su vscode -c 'python3 -m ipykernel install --user --name python3'
